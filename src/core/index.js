856ec44db08cd499d56b1d8ce94626ff215dfc21
zhongsink <zhongink@gmail.com>
2020-01-13 10:35:45 +0800

    some

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,5 @@
 import { exportCoreV2 } from './export'
 import { diyCore } from './diy'
+import { exportSample } from './util'
 
-export { diyCore, exportCoreV2 }
+export { diyCore, exportCoreV2, exportSample }
